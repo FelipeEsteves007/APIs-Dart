459689f4b4623d2c98212def17754ef3b4b4725b
Felipe Esteves <166223442+FelipeEsteves007@users.noreply.github.com>
2025-08-20 14:47:36 -0300

    add a map in the API

--- my_app/bin/main.dart
+++ my_app/bin/main.dart
@@ -1,9 +1,17 @@
 import 'package:http/http.dart';
 import 'dart:convert';
 
+import 'package:my_app/api_key.dart';
+
 void main() {
-  requestData();
-  requestDataAsync();
+  //requestData();
+  //requestDataAsync();
+  sendDataAsync({
+    "id" : "001",
+    "name" : "Flutter",
+    "lastName": "Dart",
+    "balance" : "5000"
+  });
 }
 
 requestData() {
@@ -23,10 +31,36 @@ requestData() {
   print("ULTIMA");
 }
 
-requestDataAsync() async {
+Future<List<dynamic>> requestDataAsync() async {
   String url =
       "https://gist.githubusercontent.com/FelipeEsteves007/0d965b6ba04917c3c96c7eafa544233a/raw/e169e7b861d27565dbec0b89ab2649672d1e2261/accounts.json";
   Response response = await get(Uri.parse(url));
-  print(json.decode(response.body)[0]);
-  print("Ultima");
+  return json.decode(response.body);
+}
+
+sendDataAsync(Map<String, dynamic> mapAccount) async {
+  List<dynamic> listAccounts = await requestDataAsync();
+  listAccounts.add(mapAccount);
+  String content = json.encode(listAccounts);
+  
+  String url = 
+      "https://api.github.com/gists/0d965b6ba04917c3c96c7eafa544233a";
+
+  Response response = await post(
+    Uri.parse(url),
+     headers: {
+      "Authorization" : "Bearer $githubApiKey"
+     },
+      body: json.encode({
+        "description" : "account.son",
+        "public" : true,
+        "files": {
+          "accounts.son": {
+            "content": content,
+          }
+        }
+      }),
+    );
+  print(response.statusCode);
+
 }
