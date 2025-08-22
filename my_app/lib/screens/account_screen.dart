09c92a8a68d357db1df131df7acbf706476dd1c5
unknown <felipeestevesdepaula@gmail.com>
2025-08-22 16:21:00 -0300

    add chatbot

---
+++ my_app/lib/screens/account_screen.dart
@@ -0,0 +1,61 @@
+import 'dart:io';
+import 'package:my_app/services/accountServices.dart';
+import 'package:my_app/models/account.dart';
+
+final _accountService = Accountservices();
+
+void runChatBot() async {
+  print("Bom dia! Eu sou o Lewis, assistente do Banco d'Ouro!");
+  print("Que bom te ter aqui com a gente.\n");
+
+  bool isRunning = true;
+  while (isRunning) {
+    print("Como eu posso te ajudar? (digite o número desejado)");
+    print("1 - Ver todas sua contas.");
+    print("2 - Adicionar nova conta.");
+    print("3 - Sair\n");
+
+    String? input = stdin.readLineSync();
+
+    if (input != null) {
+      switch (input) {
+        case "1":
+          {
+            await _getAllAccounts();
+            break;
+          }
+        case "2":
+          {
+            await _addExampleAccount();
+            break;
+          }
+        case "3":
+          {
+            isRunning = false;
+            print("Te vejo na próxima.");
+            break;
+          }
+        default:
+          {
+            print("Não entendi. Tente novamente.");
+          }
+      }
+    }
+  }
+}
+
+Future<void> _getAllAccounts() async {
+  List<Account> listAccounts = await _accountService.getAll();
+  print(listAccounts);
+}
+
+Future<void> _addExampleAccount() async {
+  Account example = Account(
+    id: "ID555",
+    name: "Haley",
+    lastName: "Chirívia",
+    balance: 8001,
+  );
+
+  await _accountService.addAccount(example);
+}
