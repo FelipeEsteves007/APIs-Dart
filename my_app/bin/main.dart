ef07aa893823a4b3581a8242767ecacf5a674711
unknown <felipeestevesdepaula@gmail.com>
2025-09-01 20:23:35 -0300

    add complet

--- my_app/bin/main.dart
+++ my_app/bin/main.dart
@@ -1,7 +1,17 @@
+import 'package:my_app/exceptions/transaction_excepitons.dart';
 import 'package:my_app/screens/account_screen.dart';
+import 'package:my_app/services/transaction_service.dart';
 
-void main() {
-  AccountScreen accountScreen = AccountScreen();
-  accountScreen.initializeStream();
-  accountScreen.runChatBot();
+void main() async {
+  try {
+  await TransactionService().makeTransaction(
+    idSender: "ID001",
+    idReceiver: "ID002",
+    amount: 10,
+  );
+} on InsufficientFundsException catch (e) {
+  print(e.message);
+   print("${e.cause.name} possui saldo ${e.cause.balance} menor que ${e.amount + e.taxes}");
+}
+
 }
